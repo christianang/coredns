f3b5852a14e32d2d9742e53a05e65ce37ca621c2
Christian Ang <angc@vmware.com>
2021-04-09 17:18:08 +0000

    Add missing comments to exported DNSZone structs
    Signed-off-by: Christian Ang <angc@vmware.com>

--- plugin/forwardcrd/apis/coredns/v1alpha1/dnszone_types.go
+++ plugin/forwardcrd/apis/coredns/v1alpha1/dnszone_types.go
@@ -4,11 +4,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// DNSZoneSpec represents the spec of a DNSZone
 type DNSZoneSpec struct {
 	ZoneName  string `json:"zoneName,omitempty"`
 	ForwardTo string `json:"forwardTo,omitempty"`
 }
 
+// DNSZoneStatus represents the status of a DNSZone
 type DNSZoneStatus struct {
 }
 
@@ -16,6 +18,9 @@ type DNSZoneStatus struct {
 
 // +kubebuilder:printcolumn:name="Zone Name",type=string,JSONPath=`.spec.zoneName`
 // +kubebuilder:printcolumn:name="Forward To",type=string,JSONPath=`.spec.forwardTo`
+
+// DNSZone represents a zone that should have its DNS requests forwarded to an
+// upstream DNS server within CoreDNS
 type DNSZone struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -26,6 +31,7 @@ type DNSZone struct {
 
 // +kubebuilder:object:root=true
 
+// DNSZoneList represents a list of DNSZones
 type DNSZoneList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
